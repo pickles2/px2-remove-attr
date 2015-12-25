3c7352a69f8d7268fbda48d8ac0eb7cb683eb9dc
Tomoya Koyanagi <tomk79@gmail.com>
2015-12-25 16:51:20 +0900

    initial commit.

---
+++ README.md
@@ -0,0 +1,48 @@
+tomk79/px2-remove-attr
+=========
+
+px2-remove-attr は、Pickles 2 に、HTMLドキュメントから指定した属性を削除する機能を提供します。
+
+
+## Usage - 使い方
+
+### 1. Pickles2 をセットアップ
+
+### 2. composer.json に追記
+
+```
+{
+    "require": {
+        "tomk79/px2-remove-attr": "dev-master"
+    }
+}
+```
+
+### 3. composer を更新
+
+```
+$ composer update
+```
+
+### 4. px-files/config.php に追加
+
+`$conf->funcs->processor->html` にAPI設定を追加します。
+
+```php
+<?php
+
+$conf->funcs->processor->html = [
+    // HTML属性を削除する
+    'tomk79\pickles2\remove_attr\main::exec()' ,
+];
+```
+
+
+## 開発者向け情報 - for Developer
+
+### テスト - Test
+
+```
+$ cd {$documentRoot}
+$ composer test
+```
---
+++ php/main.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Pickles2 px2-remove-attr CORE class
+ */
+namespace tomk79\pickles2\remove_attr;
+
+/**
+ * Pickles2 px2-remove-attr CORE class
+ */
+class main{
+	private $px;
+
+	/**
+	 * HTML属性削除処理の実行
+	 *
+	 * Pickles2の状態を参照し、自動的に処理を振り分けます。
+	 *
+	 * - パブリッシュする場合、DECコメントを削除します。
+	 * - プレビューの場合、DECライブラリを埋め込み、URIパラメータからDECの表示・非表示を切り替えられるようにします。
+	 *
+	 * @param object $px Picklesオブジェクト
+	 * @param object $options オプション
+	 * @return boolean true
+	 */
+	public static function exec( $px, $options = null ){
+		require_once(__DIR__.'/simple_html_dom.php');
+
+		if( !@is_array($options->attrs) ){
+			@$options->attrs = array();
+		}
+		// var_dump($options);
+
+		foreach( $px->bowl()->get_keys() as $key ){
+			$src = $px->bowl()->pull( $key );
+
+			// HTML属性を削除
+			$html = str_get_html(
+				$src ,
+				true, // $lowercase
+				true, // $forceTagsClosed
+				DEFAULT_TARGET_CHARSET, // $target_charset
+				false, // $stripRN
+				DEFAULT_BR_TEXT, // $defaultBRText
+				DEFAULT_SPAN_TEXT // $defaultSpanText
+			);
+			foreach( $options->attrs as $attr ){
+				$ret = $html->find('*['.$attr.']');
+				foreach( $ret as $retRow ){
+					// var_dump($retRow->$attr);
+					$retRow->$attr = null;
+				}
+			}
+			$src = $html->outertext;
+
+			$px->bowl()->replace( $src, $key );
+		}
+
+		return true;
+	} // exec()
+
+}
